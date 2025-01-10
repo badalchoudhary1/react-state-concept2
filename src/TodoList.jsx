36d2947ee46f33f0f46c0946458cce619aca95ff
badalchoudhary1 <badalror10@gmail.com>
2025-01-10 10:47:52 +0530

    add todolist

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,67 +1,133 @@
-import { useState } from "react"
-import { v4 as uuidv4 } from 'uuid';
-export default function TodoList(){
-    let [todos,setTodos]=useState([{task:"Sample-task",id:uuidv4()}]);
-    let [newTodo,setNewTodo]=useState("");
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-    let addNewTask =()=>{
-        setTodos((prevTodos)=>{
-            return [...prevTodos,{task:newTodo,id:uuidv4()}];
-        });
-        setNewTodo("");
-        
-    };
-    let updateTodoValue =(event)=>{
-        setNewTodo(event.target.value);
-        
-    };
-    let deleteTodo=(id)=>{
-        setTodos((prevTodos)=>todos.filter((prevTodos)=>prevTodos.id != id));
-        
-    };  
-    let upperCaseAll =()=>{
-        setTodos((prevTodos)=>(
-            prevTodos.map((todo)=>{
-                return {
-                    ...todo,task:todo.task.toUpperCase(),
-                };
-            })
-        ));
-    };
-    let upperCaseOne=(id)=>{
-        setTodos((prevTodos)=>(
-            prevTodos.map((todo)=>{
-                if (todo.id == id){
-                    return {
-                        ...todo,task:todo.task.toUpperCase(),
-                    };  
-                }
-              else{
-                return todo;
-              }
-            })
-        ));
-        
-    }
 
-    return(
-        <div>
-            <input placeholder="add a task" value={newTodo} onChange={updateTodoValue}></input>
-            
-            <button onClick={addNewTask}>Add Task</button>
-            <br /><br /><br /><br />
+export default function TodoList() {
+  let [todos, setTodos] = useState([
+    { task: "Sample-task", id: uuidv4(), isDone: false },
+  ]);
+  let [newTodo, setNewTodo] = useState("");
 
-            <h2>Todo List</h2>
-            <ul>
-                {todos.map((todo)=>(
-                    <li key={todo.id}>
-                    <span>{todo.task}</span>
-                    <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-                    <button onClick={() => upperCaseOne(todo.id)}>upperCase one</button>
-                    </li>
-                ))}
-            </ul>
-            <button onClick={upperCaseAll}>upperCase all</button>
-        </div>
+  let addNewTask = () => {
+    setTodos((prevTodos) => {
+      return [...prevTodos, { task: newTodo, id: uuidv4(), isDone: false }];
+    });
+    setNewTodo("");
+  };
+
+  let updateTodoValue = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  let deleteTodo = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.filter((prevTodo) => prevTodo.id !== id)
     );
+  };
+
+  let upperCaseAll = () => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        return {
+          ...todo,
+          task: todo.task.toUpperCase(),
+        };
+      })
+    );
+  };
+
+  let upperCaseOne = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            task: todo.task.toUpperCase(),
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
+  let markAsDone = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isDone: true,
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
+  let markAsAll = () => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => {
+        return {
+          ...todo,
+          isDone: true,
+        };
+      })
+    );
+  };
+
+  return (
+    <div className="todo-container">
+      <h2 className="title">Todo List</h2>
+      <div className="input-container">
+        <input
+          placeholder="Add a task"
+          value={newTodo}
+          onChange={updateTodoValue}
+          className="input"
+        />
+        <button onClick={addNewTask} className="btn add-btn">
+          Add Task
+        </button>
+      </div>
+
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li key={todo.id} className={`todo-item ${todo.isDone ? "done" : ""}`}>
+            <span>{todo.task}</span>
+            <div className="actions">
+              <button
+                onClick={() => deleteTodo(todo.id)}
+                className="btn delete-btn"
+              >
+                Delete
+              </button>
+              <button
+                onClick={() => upperCaseOne(todo.id)}
+                className="btn uppercase-btn"
+              >
+                Uppercase
+              </button>
+              <button
+                onClick={() => markAsDone(todo.id)}
+                className="btn done-btn"
+              >
+                Done
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      <div className="bulk-actions">
+        <button onClick={upperCaseAll} className="btn bulk-btn">
+          Uppercase All
+        </button>
+        <button onClick={markAsAll} className="btn bulk-btn">
+          Mark All as Done
+        </button>
+      </div>
+    </div>
+  );
 }
